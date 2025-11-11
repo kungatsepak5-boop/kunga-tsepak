c20b58c59aaa5dc9100f46bbe860277d251ef765
kunga tsepak <kungatsepak5@gmail.com>
2025-11-11 17:42:30 +0530

    these are my task

--- Ass3 task 1.py
+++ Ass3 task 1.py
@@ -1,6 +1,10 @@
 num=int(input("Enter a number:"))
 fac=1
-while num>0:
-    fac*=num
-    num-=1
-print(fac)
+if num <0:
+    print ("invalid Input")
+else:
+    number=num
+    while num>0:
+        fac*=num
+        num-=1
+print(f"Factorial of {number} is :  {fac}")
