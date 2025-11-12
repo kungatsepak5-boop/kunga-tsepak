f7899e8fb434baa7ab828ecfe313c7eda94df452
kunga tsepak <kungatsepak5@gmail.com>
2025-11-12 19:53:01 +0530

    these are my task

--- Ass3 task 1.py
+++ Ass3 task 1.py
@@ -1,4 +1,4 @@
-num=int(input("Enter a number:"))
+num=int(input("Enter a number: "))
 fac=1
 if num <0:
     print ("invalid Input")
---
+++ Ass3 task 2.py
@@ -0,0 +1,8 @@
+import math 
+num=int(input("Enter a number: "))
+square_root= math.sqrt(num)
+log= math.log(num)
+sine=math.sin(num)
+print (f"square root of {num} : {square_root}")
+print (f"logarithm : {log}")
+print (f"sine : {sine}")
